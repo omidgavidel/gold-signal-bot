f94df6e3134347ee7a8468e445ed734f76e5f5f9
omidgavidel <omid.gavidel2000@gmail.com>
2026-07-22 19:14:27 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,15 @@
-print("Gold Signal Bot Started")
+import requests
+
+TOKEN = " 8479783686:AAEjLz-nc8jULTTnHOJQeMKMt06GqeFLJak"
+CHAT_ID = "5840426117"
+
+message = "ربات طلا فعال شد ✅"
+
+url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+
+requests.get(url, params={
+    "chat_id": CHAT_ID,
+    "text": message
+})
+
+print("Message sent")
